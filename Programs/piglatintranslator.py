d7b410bffecfc3380200ca54e4da0f50b5181d5f
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 12:23:40 -0500

    update

--- Programs/piglatintranslator.py
+++ Programs/piglatintranslator.py
@@ -4,8 +4,8 @@ print ("Welcome to the Pig Latin Translator!")
 #original word input 
 original = raw_input("Enter a word: ")
 
-#check if word has letters, if so displaying it
-if len(original) > 0:
+#check if word has letters and no numbers, if so then displaying it
+if len(original) > 0 and original.isalpha():
   print (original)
 else:
   print ("empty")
