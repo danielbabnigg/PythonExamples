f979b375b60b3db4c3f601df985d5d0ae4b1efe4
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 10:53:12 -0500

    ghfgh

--- Snippets/indexexample.py
+++ Snippets/indexexample.py
@@ -1,6 +1,6 @@
 #simple index operations
 fifth_letter = "hsIODNFISDJOAI"[4]
 print (fifth_letter)
-print len(fifth_letter)
+print .len(fifth_letter)
 print .lower(fifth_letter)
 print .upper(fifth_letter)
