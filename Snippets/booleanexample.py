d1b324bd5d8c3d5b3fb736fe95cb888ab4961dcc
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 11:45:42 -0500

    update

--- Snippets/booleanexample.py
+++ Snippets/booleanexample.py
@@ -10,3 +10,34 @@ num2 = 8
 addition = int(num1) + int(num2)
 print (addition)
 print ('Value equals to ' + str(addition))
+
+#comparators
+>>> 2 == 2
+True
+>>> 2 == 5
+False
+
+>>> 2 != 5
+True
+>>> 2 != 2
+False
+
+>>> 2 < 5
+True
+>>> 5 < 2
+False
+
+>>> 2 <= 2
+True
+>>> 5 <= 2
+False
+
+>>> 5 > 2
+True
+>>> 2 > 5
+False
+
+>>> 5 >= 5
+True
+>>> 2 >= 5
+False
