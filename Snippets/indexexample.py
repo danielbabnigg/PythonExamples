872e7e37934c7e8c2b9857716a4dcc48fcbfb9fc
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 11:03:35 -0500

    ji

--- Snippets/indexexample.py
+++ Snippets/indexexample.py
@@ -1,6 +1,8 @@
 #simple index operations
 fifth_letter = "hsIODNFISDJOAI"[4]
 print (fifth_letter)
-print .len(fifth_letter)
-print (fifth_letter).lower()
-print (fifth_letter).upper()
+
+letters = "hsIODNFISDJOAI"
+print len(letters)
+print (letters).lower()
+print (letters).upper()
