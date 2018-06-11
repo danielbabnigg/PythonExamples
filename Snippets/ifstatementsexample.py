0bd6ae4b7ad1e5fc8b7971886bd15d814c64525b
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 12:09:12 -0500

    update

--- Snippets/ifstatementsexample.py
+++ Snippets/ifstatementsexample.py
@@ -1,1 +1,15 @@
 #general if statement
+def grade_converter(grade):
+    if grade >= 90:
+        return "A"
+    elif grade >= 80:
+        return "B"
+    elif grade >= 70:
+        return "C"
+    elif grade >= 65:
+        return "D"
+    else:
+        return "F"
+print (grade_converter(90))
+print (grade_converter(85))
+print (grade_converter(59))
