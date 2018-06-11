31b6ca93786c56bbbb7c04eed29e6634974a416a
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 12:21:56 -0500

    update

---
+++ Programs/piglatintranslator.py
@@ -0,0 +1,11 @@
+#title
+print ("Welcome to the Pig Latin Translator!")
+
+#original word input 
+original = raw_input("Enter a word: ")
+
+#check if word has letters, if so displaying it
+if len(original) > 0:
+  print (original)
+else:
+  print ("empty")
