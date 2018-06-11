61cddea06439b4a5485b5ab08f4bc08350e2e203
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 11:34:17 -0500

    update

--- Snippets/dateandtimeexample.py
+++ Snippets/dateandtimeexample.py
@@ -1,9 +1,12 @@
 #basic date printing
 from datetime import datetime
 now = datetime.now()
+
 current_year = now.year
 current_month = now.month
 current_day = now.day
 print (current_year)
 print (current_month)
 print (current_day)
+
+print ("%02d/%02d/%04d" % (now.day, now.month, now.year))
