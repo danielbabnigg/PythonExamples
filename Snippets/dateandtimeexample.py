c8c43ef9c7ef9f5862c8c2d8589e1ee199fe2b3a
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 11:30:51 -0500

    update

--- Snippets/dateandtimeexample.py
+++ Snippets/dateandtimeexample.py
@@ -4,6 +4,6 @@ now = datetime.now()
 current_year = now.year
 current_month = now.month
 current_day = now.day
-print current_year
-print current_month
-print current_day
+print (current_year)
+print (current_month)
+print (current_day)
