516fa82e0c1718c7d48220646a8ddb0487a46537
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 12:45:30 -0500

    update

---
+++ Snippets/functionsexample.py
@@ -0,0 +1,12 @@
+#basic function
+def breakfast():
+    """Lists breakfast foods"""
+    print ("Eggs, bacon, toast")
+breakfast()
+
+#calling functions
+def square(n):
+    squared = n ** 2
+    print "%d squared is %d" % (n, squared)
+    return squared
+square(6)
