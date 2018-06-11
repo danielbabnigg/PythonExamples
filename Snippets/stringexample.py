21422686f6db6fc95767bd396b37f5ec4067f78e
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 11:22:24 -0500

    update

--- Snippets/stringexample.py
+++ Snippets/stringexample.py
@@ -9,7 +9,7 @@ print (str(pi))
 print ("There\'s a snake!")
 
 #variable string shortcuts
-name = Daniel
-age = 15
-color = orange
+name = "Daniel"
+age = "15"
+color = "orange"
 print ("Hello, my name% is %s and I am %s years old, my favorite color is %s." % (name,age,color))
