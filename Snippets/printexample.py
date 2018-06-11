8bd9e99773403525284f01c3ca5bfb2530ad9cf2
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 10:14:47 -0500

    update

--- Snippets/printexample.py
+++ Snippets/printexample.py
@@ -1,2 +1,10 @@
-x = 3
-print (x)
+#string prints
+print "Hey, you!"
+
+print ("Hello!")
+
+print ('Hey ' + 'Daniel!')
+
+#variable prints
+variable1 = hi
+print (variable1)
---
+++ Snippets/variableexample.py
@@ -0,0 +1,34 @@
+#string variables
+today = "June 6th, 2018"
+print (today)
+
+haiku = """hi
+yes
+no"""
+print (haiku)
+
+#simple math variables
+
+integer = -5
+print (integer)
+
+product = 6 * 12
+print (product)
+quotient = 6/4
+print (quotient)
+quotientfloat = 6./4
+print (quotientfloat)
+remainder = 125 % 6
+print (remainder)
+
+#shortcuts in math variables
+
+annual_rainfall = 6
+december_rainfall = 7
+annual_rainfall += december_rainfall
+print (annual_rainfall)
+
+float1 = 10.
+print (float1)
+float4 = 1.5e2
+print (float4)
