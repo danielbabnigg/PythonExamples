935955f6f7cf439d3d61ca0e3373b3260b77a2e5
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-11 10:30:49 -0500

    update

--- Snippets/booleanexample.py
+++ Snippets/booleanexample.py
@@ -1,3 +1,12 @@
 #general booleans
 a = True
+print (a)
 b = False
+print (b)
+
+#operational booleans
+num1 = 6
+num2 = 8
+addition = int(num1) + int(num2)
+print (addition)
+print "Value is " + str(addition)
--- Snippets/printexample.py
+++ Snippets/printexample.py
@@ -5,6 +5,10 @@ print ("Hello!")
 
 print ('Hey ' + 'Daniel!')
 
+ans = 6
+solution = "The product was " + str(ans)
+print (solution)
+
 #variable prints
 variable1 = hi
 print (variable1)
