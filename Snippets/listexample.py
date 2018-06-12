43d7ce6b4099c2c4e29787e6cf6c84dccbe109c2
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 09:43:20 -0500

    update

--- Snippets/listexample.py
+++ Snippets/listexample.py
@@ -6,3 +6,15 @@ if len(zoo_animals) > 3:
   print ("The second animal at the zoo is the " + zoo_animals[1])
   print ("The third animal at the zoo is the " + zoo_animals[2])
   print ("The fourth animal at the zoo is the " + zoo_animals[3])
+
+#changing lists
+zoo_animals = ["pangolin", "cassowary", "sloth", "hippo"]
+zoo_animals[0] = "tiger"
+
+#adding to lists
+box = []
+box.append("cookies")
+
+#length of lists
+bag = ["towel", "phone", "paper"]
+print (len(bag))
