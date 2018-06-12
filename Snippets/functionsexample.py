03018131937efa8989c762ce8e9bb0b51dc7e7c3
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 09:01:59 -0500

    update

--- Snippets/functionsexample.py
+++ Snippets/functionsexample.py
@@ -52,3 +52,11 @@ def shut_down(s):
   else:
     return "Sorry"
 print (shut_down("no"))
+
+#example with if and or
+def distance_from_zero(num):
+  if type(num) == int or type(num) == float:
+    return abs(num)
+  else:
+    return "Nope"
+print distance_from_zero(-5.234)
