3c075e7eda124e19adbc1b1e98ad7266484d63db
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 10:05:16 -0500

    update

--- Programs/calculator.py
+++ Programs/calculator.py
@@ -1,15 +1,29 @@
+print ("Available Operations: add, subtract, multiply, divide, square, power")
 operation = (input("Which operation? "))
-num1 = (input("First number? "))
-num2 = (input("Second number? "))
 
 if (operation == "addition" or operation == "add"):
+    num1 = (input("First number? "))
+    num2 = (input("Plus what number? "))
     answer = (int(num1) + int(num2))
 elif (operation == "subtraction" or operation == "subtract"):
+    num1 = (input("First number? "))
+    num2 = (input("Minus what number? "))
     answer = (int(num1) - int(num2))
 elif (operation == "multiplication" or operation == "multiply"):
+    num1 = (input("First number? "))
+    num2 = (input("Times what number? "))
     answer = (int(num1) * int(num2))
 elif (operation == "division" or operation == "divide"):
+    num1 = (input("First number? "))
+    num2 = (input("Divided by what number? "))
     answer = (int(num1) / int(num2))
+elif (operation == "square"):
+    num1 = (input("First number? "))
+    answer = (int(num1) ** 2
+elif (operation == "power"):
+    num1 = (input("First number? "))
+    num2 = (input("To the power of what number? "))
+    answer = (int(num1) ** int(num2))
 else:
     print ("Invalid Operation")
 print ("Your answer is %s" % (answer))
