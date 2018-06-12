dd35dcbd503685497bf1fe1b20ff108b1e6cc0f5
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 08:41:03 -0500

    update

--- Snippets/functionsexample.py
+++ Snippets/functionsexample.py
@@ -25,3 +25,20 @@ def by_three(number):
     return cube(number)
   else:
     return False
+
+#functions with more complicated math
+def biggest_number(*args):
+  print (max(args))
+  return max(args)
+    
+def smallest_number(*args):
+  print (min(args))
+  return min(args)
+
+def distance_from_zero(arg):
+  print (abs(arg))
+  return abs(arg)
+
+biggest_number(-10, -5, 5, 10)
+smallest_number(-10, -5, 5, 10)
+distance_from_zero(-10)
--- Snippets/importexample.py
+++ Snippets/importexample.py
@@ -1,3 +1,8 @@
 #math imports
 import math
 print (math.sqrt(100))
+
+from math import sqrt
+print (sqrt(25))
+
+from math import *
