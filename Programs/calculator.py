0604105956d8db5f3b13b29756e9150bdf052d2c
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 09:58:40 -0500

    update

---
+++ Programs/calculator.py
@@ -0,0 +1,15 @@
+operation = (input("Which operation? "))
+num1 = (input("First number? "))
+num2 = (input("Second number? "))
+
+if (operation == "addition" or operation == "add"):
+    answer = (int(num1) + int(num2))
+elif (operation == "subtraction" or operation == "subtract"):
+    answer = (int(num1) - int(num2))
+elif (operation == "multiplication" or operation == "multiply"):
+    answer = (int(num1) * int(num2))
+elif (operation == "division" or operation == "divide"):
+    answer = (int(num1) / int(num2))
+else:
+    print ("Invalid Operation")
+print (answer)
