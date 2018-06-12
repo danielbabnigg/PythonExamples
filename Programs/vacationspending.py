ae95c89c6e4cc3b4544f7e975aa4d592a75a4fa0
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 09:19:13 -0500

    update

--- Programs/vacationspending.py
+++ Programs/vacationspending.py
@@ -1,21 +1,21 @@
 def hotel_cost(nights):
-  return 140 * nights
+  return (140 * nights)
 
 def plane_ride_cost(city):
-  if city == "Charlotte":
+  if (city == "Charlotte"):
     return 183
-  elif city == "Tampa":
+  elif (city == "Tampa"):
     return 220
-  elif city == "Pittsburgh":
+  elif (city == "Pittsburgh"):
     return 222
-  elif city == "Los Angeles":
+  elif (city == "Los Angeles"):
     return 475
 
 def rental_car_cost(days):
-  cost = days * 40
-  if days >= 7:
+  cost = (days * 40)
+  if (days >= 7):
     cost -= 50
-  elif days >= 3:
+  elif (days >= 3):
     cost -= 20
   return cost
 
@@ -26,4 +26,4 @@ a = (input("Enter you vacation spot: "))
 b = (input("How many days?: "))
 c = (input("Spending Money: "))
 
-print (trip_cost("a", int(b), int(c)))
+print (trip_cost(int(a), int(b), int(c)))
