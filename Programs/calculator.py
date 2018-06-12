661fa55b9ea1c9bed1a73817460ae88378e5ebef
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 11:23:20 -0500

    update

--- Programs/calculator.py
+++ Programs/calculator.py
@@ -4,26 +4,26 @@ operation = (input("Which operation? "))
 if (operation == "addition" or operation == "add"):
     num1 = (input("First number? "))
     num2 = (input("Plus what number? "))
-    answer = (int(num1) + int(num2))
+    answer = (float(num1) + float(num2))
 elif (operation == "subtraction" or operation == "subtract"):
     num1 = (input("First number? "))
     num2 = (input("Minus what number? "))
-    answer = (int(num1) - int(num2))
+    answer = (float(num1) - float(num2))
 elif (operation == "multiplication" or operation == "multiply"):
     num1 = (input("First number? "))
     num2 = (input("Times what number? "))
-    answer = (int(num1) * int(num2))
+    answer = (float(num1) * float(num2))
 elif (operation == "division" or operation == "divide"):
     num1 = (input("First number? "))
     num2 = (input("Divided by what number? "))
-    answer = (int(num1) / int(num2))
+    answer = (float(num1) / float(num2))
 elif (operation == "square"):
     num1 = (input("First number? "))
-    answer = (int(num1) ** 2)
+    answer = (float(num1) ** 2)
 elif (operation == "power"):
     num1 = (input("First number? "))
     num2 = (input("To the power of what number? "))
-    answer = (int(num1) ** int(num2))
+    answer = (float(num1) ** float(num2))
 else:
     print ("Invalid Operation")
 print ("Your answer is %s" % (answer))
