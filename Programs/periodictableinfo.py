c61ae35914e3d49c36aa26c8cfe6fbdd4201a7f8
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-12 10:14:13 -0500

    update

---
+++ Programs/periodictableinfo.py
@@ -0,0 +1,43 @@
+print ("Welcome, enter your element below.")
+element = (input("Element: "))
+
+if (element = Hydrogen):
+    print ("H")
+    print ("Gas")
+    print ("1.008")
+if (element = Helium):
+    print ("He")
+    print ("Gas")
+    print ("4.0026")
+if (element = Lithium):
+    print ("Li")
+    print ("Solid")
+    print ("6.94")
+if (element = Beryllium):
+    print ("Be")
+    print ("Solid")
+    print ("9.0122")
+if (element = Boron):
+    print ("B")
+    print ("Solid")
+    print ("10.81")
+if (element = Carbon):
+    print ("C")
+    print ("Solid")
+    print ("12.011")
+if (element = Nitrogen):
+    print ("N")
+    print ("Gas")
+    print ("14.007")
+if (element = Oxygen):
+    print ("O")
+    print ("Gas")
+    print ("15.999")
+if (element = Fluorine):
+    print ("F")
+    print ("Gas")
+    print ("18.998")
+if (element = Neon):
+    print ("Ne")
+    print ("Gas")
+    print ("20.180")
