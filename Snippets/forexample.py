f2d26a495638a3f80c529a9459ed455357fc7704
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-13 08:54:12 -0500

    update

--- Snippets/forexample.py
+++ Snippets/forexample.py
@@ -2,4 +2,29 @@
 list = [1,2,3,4,5,6,7,8,9]
 for number in list:
     print (number ** 2)
-    
+
+#if statements in for-loops
+a = [1, 2, 5, 8, 9, 21, 32, 30, 12]
+for number in a:
+    if number % 2 != 0:
+        print (number)
+    print ("All odd numbers printed")
+
+#more complex loops
+total = 0
+prices = {
+    "bread": 5,
+    "apple": 3,
+    "roll": 4
+}
+amount = {
+    "bread": 8,
+    "apple": 5,
+    "roll": 2
+}
+for food in prices:
+    print (food)
+    print ("price: %s" % prices[food])
+    print ("amount: %s" % amount[food])
+    total += (prices[food] * amount[food])
+print (total)
