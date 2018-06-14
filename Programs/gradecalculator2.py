11a0ba570023848be10265269431f0c6f4f3f8b8
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-14 10:10:09 -0500

    update

--- Programs/gradecalculator2.py
+++ Programs/gradecalculator2.py
@@ -1,10 +1,10 @@
 categories = {}
-while True:
-    if cat1 != "stop":
-        cat1 = str(input("What is your first category? "))
-        catk1 = int(input("What percentage is it of your grade? "))
-        categories[cat1] = catk1
+while cat1 != "stop":
+    cat1 = str(input("What is your first category? "))
+    catk1 = int(input("What percentage is it of your grade? "))
+    categories[cat1] = catk1
     else:
         break
+
 for i in categories:
     print (i, "-", categories[i])
