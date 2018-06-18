ac073076f7d0ae48bad442125c12141805a9ed95
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-18 10:53:20 -0500

    update

---
+++ Snippets/dictionaryexample.py
@@ -0,0 +1,8 @@
+#calling items in a dictionary
+dict1 = {"Hi": 1, "No": 2, "Yes": 3, "Ok": 4}
+print (dict1.items())
+
+#callings keys and values from a dictionary
+dict2 = {"Animal": 1, "Sheep": 2, "Cow": 3, "Hippo": 4}
+print (dict2.keys())
+print (dict2.values())
