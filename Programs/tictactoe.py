adda1fc809b34146464115ac84fdba3e391c8721
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-18 10:17:55 -0500

    update

--- Programs/tictactoe.py
+++ Programs/tictactoe.py
@@ -13,10 +13,13 @@ def print_board(board):
     print (" ".join(row))
 
 turnsleft = 5
+isdone = 0
 print ("Turns left:", turnsleft)
 print_board(board)
 
 while turnsleft > 0:
+    if isdone == 1:
+        break
     guessrow = int(input("What row? "))
     guesscol = int(input("What column? "))
     if guessrow not in range(4) or guesscol not in range(4):
@@ -33,86 +36,6 @@ while turnsleft > 0:
         print ("Turns left:", (turnsleft))
     else:
         board[guessrow-1][guesscol-1] = "X"
-        if (board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X"):
-            print ("")
-            print_board(board)
-            print ("Congratulations, you win!")
-            break
-        elif (board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
-        elif (board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
-        elif (board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
-        elif (board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
-        elif (board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
-        elif (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
-        elif (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
-        elif (board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O"):
-            print ("")
-            print_board(board)
-            print ("Sorry, you lose!")
-            break
         randrow = randint(0,2)
         randcol = randint(0,2)
         while turnsleft > 1:
@@ -127,10 +50,92 @@ while turnsleft > 0:
                 randcol = randint(0,2)
             else:
                 board[randrow][randcol] = "O"
+                if (board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    isdone = 1
+                    break
+                elif (board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    break
+                elif (board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    break
+                elif (board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    break
+                elif (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    break
+                elif (board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    break
+                elif (board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    break
+                elif (board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X"):
+                    print ("")
+                    isdone = 1
+                    print ("Congratulations, you win!")
+                    break
+                elif (board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
+                elif (board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
+                elif (board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
+                elif (board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
+                elif (board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
+                elif (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
+                elif (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
+                elif (board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O"):
+                    print ("")
+                    isdone = 1
+                    print ("Sorry, you lose!")
+                    break
                 break
-        print ("")
-        print ("Turns left:", (turnsleft - 1))
-        turnsleft = turnsleft - 1
+        if isdone == 0:
+            print ("")
+            print ("Turns left:", (turnsleft - 1))
+            turnsleft = turnsleft - 1
     print_board(board)
 else:
     print ("Game Over, you lost!")
