cf91ec897c7f808338c826510bc620285c8507fa
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-19 10:57:07 -0500

    update

--- Snippets/fileexample.py
+++ Snippets/fileexample.py
@@ -1,10 +1,10 @@
 #opening and reading files
-my_file = open("hello.txt", "r")
+my_file = open("test.txt", "r")
 print (my_file.read())
 my_file.close()
 print ("")
 
-my_file = open("hello.txt", "r")
+my_file = open("test.txt", "r")
 print (my_file.readline())
 print (my_file.readline())
 print (my_file.readline())
@@ -13,20 +13,20 @@ print (my_file.readline())
 my_file.close()
 print ("")
 
-my_file = open("hello.txt", "r")
+my_file = open("test.txt", "r")
 print (my_file.readlines())
 my_file.close()
 print ("")
 
 #writing in a file
-my_file1 = open("hello.txt", "a")
+my_file1 = open("test.txt", "a")
 my_file1.write("\n" + "understood")
 my_file1.close()
-my_file1 = open("hello.txt", "r")
+my_file1 = open("test.txt", "r")
 print (my_file1.read())
 my_file1.close()
 
 #creating a new file and writing to it
-my_file2 = open("hello1.txt", "x")
+my_file2 = open("test1.txt", "x")
 my_file2.write("hi ok")
 my_file2.close()
