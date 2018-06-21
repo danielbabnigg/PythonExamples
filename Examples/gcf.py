03b2239af46127ff1ab59a1f258f87fc5036c1de
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-21 11:41:11 -0500

    update

---
+++ Examples/gcf.py
@@ -0,0 +1,26 @@
+def gcf(num1, num2):
+    list1 = []
+    list2 = []
+    factors = []
+    for i in range(1, num1 + 1):
+        if num1 % i == 0:
+            list1.append(i)
+        else:
+            list1 = list1
+    for i in range(1, num2 + 1):
+        if num2 % i == 0:
+            list2.append(i)
+        else:
+            list2 = list2
+    for a in list1:
+        for i in list2:
+            if a == i:
+                factors.append(a)
+    if factors == []:
+        print ("")
+        print ("There is no greatest common factor between %s and %s!" % (num1, num2))
+    else:
+        factors.reverse()
+        print ("")
+        print ("The greatest common factor between %s and %s is %s!" % (num1, num2, factors[0]))
+gcf(int(input("First number? ")), int(input("Second number? ")))
