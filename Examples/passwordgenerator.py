4784a7557da1905312ad6e3e8f8c7a939339b17a
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-21 12:17:17 -0500

    update

---
+++ Examples/passwordgenerator.py
@@ -0,0 +1,14 @@
+from random import randint
+
+def passgen(str, num, extra):
+    password = ""
+    randnum = randint(1, 3)
+    if randint == 1:
+        for i in range(0, len(str)):
+            print (i)
+    if randint == 2:
+        for i in range(0, len(str)):
+            print (i)
+    if randint == 3:
+        for i in range(0, len(str)):
+            print (i)
