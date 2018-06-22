a41e21db0ddd9af32255cc0709e0bc74cccffe0c
dbabnigg <dbabnigg@csi355869.bio.anl.gov>
2018-06-22 11:30:48 -0500

    update

---
+++ Examples/guessnumber.py
@@ -0,0 +1,14 @@
+from random import randint
+
+n = randint(1, 99)
+guess = int(input("What is your guess? "))
+
+while n != guess:
+    if n > guess:
+        print ("Too low!")
+        guess = int(input("What is your guess? "))
+    if n < guess:
+        print ("Too high!")
+        guess = int(input("What is your guess? "))
+else:
+    print ("You guessed it!")
